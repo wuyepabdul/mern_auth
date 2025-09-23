35be39a8faf3a200638fcc099d9fbc1127fa738a
wuyepabdul <wuyepabdul@gmail.com>
2025-09-23 11:00:44 +0100

    Create auth route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { connectDb } from "./db/connectDb.js";
 import dotenv from "dotenv";
+import authRoutes from "../backend/routes/auth.route.js";
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,7 @@ app.get("/", (req, res) => {
   res.send("API");
 });
 
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
   connectDb();
