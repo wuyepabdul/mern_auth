aa400f6e5a0d840b6a588e1cb6a43bac9e3decf5
wuyepabdul <wuyepabdul@gmail.com>
2025-09-23 11:07:20 +0100

    Create auth controller

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,9 +1,12 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller";
 
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
-  res.send("signup");
-});
+router.get("/signup", signup);
+
+router.post("/login", login);
+
+router.post("/logout", logout);
 
 export default router;
