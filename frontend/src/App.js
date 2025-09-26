30176b409588daaa594b323ed55f9625030c82d4
wuyepabdul <wuyepabdul@gmail.com>
2025-09-26 20:55:47 +0100

    Install zustand and axios, set up zustand store and add signup function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { SignUpPage } from "./pages/SignUpPage";
 import { LoginPage } from "./pages/LoginPage";
+import { EmailVerificationPage } from "./pages/EmailVerificationPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Route path="/" element={"home"} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/verify-email" element={<EmailVerificationPage />} />
       </Routes>
     </div>
   );
