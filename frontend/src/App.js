f654bbe4ba94337732f573af77770e3dfdf578fe
wuyepabdul <wuyepabdul@gmail.com>
2025-09-27 11:31:11 +0100

    Add Check if user is authenticated functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,17 @@ import { SignUpPage } from "./pages/SignUpPage";
 import { LoginPage } from "./pages/LoginPage";
 import { EmailVerificationPage } from "./pages/EmailVerificationPage";
 import { Toaster } from "react-hot-toast";
+import { useAuthStore } from "./store/authStore";
+import { useEffect } from "react";
 
 function App() {
+  const { isCheckingAuth, checkAuth, isAuthenticated, user } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+  console.log("isAuthenticated", isAuthenticated);
+  console.log("user", user);
   return (
     <div className="min-h-screen bg-gradient-to-r from-green-900 via-green-400 to-green-900 flex items-center justify-center relative overflow-hidden">
       <Routes>
--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -49,7 +49,21 @@ export const useAuthStore = create((set) => ({
         error: error.response.data.message || "Error verifying email",
         isLoading: false,
       });
-      throw error
+      throw error;
+    }
+  },
+
+  checkAuth: async () => {
+    set({ isCheckingAuth: true, error: null });
+    try {
+      const response = await axios.get(`${API_URL}/check-auth`);
+      set({
+        user: response.data.user,
+        isAuthenticated: true,
+        isCheckingAuth: false,
+      });
+    } catch (error) {
+      set({ error: null, isCheckingAuth: false, isAuthenticated: false });
     }
   },
 }));
