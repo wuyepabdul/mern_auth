5062cecc15f7996897e4dffb5c42137b47a695d6
wuyepabdul <wuyepabdul@gmail.com>
2025-09-28 12:50:17 +0100

    Add .env file in frontend

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,8 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth";
+// const API_URL = "http://localhost:5000/api/auth";
+const API_URL = import.meta.env.REACT_APP_BACKEND_BASE_URL;
 
 axios.defaults.withCredentials = true; //axios puts the cookies in the request headers for every request
 
