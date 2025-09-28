d1e0d6d1ee5d4c7a5ba4956e6af9add208424917
wuyepabdul <wuyepabdul@gmail.com>
2025-09-28 20:56:33 +0100

    Add production url in env file

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,15 @@ app.get("/", (req, res) => {
   res.send("API");
 });
 
-app.use(cors({ origin: `${process.env.CLIENT_URL}`, credentials: true }));
+app.use(
+  cors({
+    origin: [
+      `${process.env.CLIENT_URL_PRODUCTION}`,
+      `${process.env.CLIENT_URL}`,
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json()); // allows us to parse incoming requests from req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
