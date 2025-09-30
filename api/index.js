7c4ae72e2693dc82eb050e5cae32943f4fab9849
wuyepabdul <wuyepabdul@gmail.com>
2025-09-30 14:22:58 +0100

    Update vercel

--- api/index.js
+++ api/index.js
@@ -4,12 +4,10 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
-const __dirname = path.resolve();
 
 app.get("/", (req, res) => {
   res.send("Welcome to MERN Advanced Auth API");
@@ -29,14 +27,6 @@ app.use(express.json()); // allows us to parse incoming requests from req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
 
 app.use("/api/auth", authRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-}
 
 connectDb();
 
