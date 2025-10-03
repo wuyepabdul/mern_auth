75ed933d5e4ee014e5c95cb38aea691dd42e822d
wuyepabdul <wuyepabdul@gmail.com>
2025-10-03 11:46:43 +0100

    Setup app for deployment on render

--- api/index.js
+++ api/index.js
@@ -4,34 +4,23 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.get("/", (req, res) => {
   res.send(`Welcome to MERN Advanced Auth API at :  ${process.env.CLIENT_URL}`);
 });
 
-const corsConfig = {
-  origin: process.env.CLIENT_URL,
-  credentials: true,
-};
-
-const allowedOrigins = [process.env.CLIENT_URL];
-
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL],
+    credentials: true,
+  })
+);
 
 app.use((req, res, next) => {
   console.log("Origin:", req.headers.origin);
@@ -42,6 +31,14 @@ app.use(express.json()); // allows us to parse incoming requests from req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
 
 app.use("/api/auth", authRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 connectDb();
 
