cd3f1eca4b07077b3e0ad6a1896db7016bb9ddd9
wuyepabdul <wuyepabdul@gmail.com>
2025-10-07 10:19:02 +0100

    Edit index js and vercel files for deployment

--- backend/index.js
+++ backend/index.js
@@ -4,41 +4,42 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
+// import path from "path";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 app.get("/", (req, res) => {
   res.send(`Welcome to MERN Advanced Auth API at :  ${process.env.CLIENT_URL}`);
 });
 
-app.use(
-  cors({
-    origin: [process.env.CLIENT_URL],
-    credentials: true,
-  })
-);
+const corsConfig = {
+  origin: [process.env.CLIENT_URL],
+  credentials: true,
+  method: ["GET", "POST", "PUT", "DELETE"],
+};
+app.options("*",cors(corsConfig));
+app.use(cors(corsConfig));
 
 app.use((req, res, next) => {
   console.log("Origin:", req.headers.origin);
   next();
 });
 
-app.use(express.json()); // allows us to parse incoming requests from req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
+app.use(express.json()); // allows us to parse incoming requests from req.body
 
 app.use("/api/auth", authRoutes);
 
-if (process.env.NODE_ENV === "production") {
+/* if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
 
   app.get("/*splat", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-}
+} */
 
 connectDb();
 
--- test.js
+++ test.js
@@ -1,10 +1,58 @@
-const obj = {
-  a: 10,
-  b: () => console.log(this.a),
-  c() {
-    console.log(this.a);
-  },
-};
+// const obj = {
+//   a: 10,
+//   b: () => console.log(this.a),
+//   c() {
+//     console.log(this.a);
+//   },
+// };
 
-obj.b();
-obj.c();
+// obj.b();
+// obj.c();
+
+import express from "express";
+import { connectDb } from "./db/connectDb.js";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+app.get("/", (req, res) => {
+  res.send(`Welcome to MERN Advanced Auth API at :  ${process.env.CLIENT_URL}`);
+});
+
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL],
+    credentials: true,
+  })
+);
+
+app.use((req, res, next) => {
+  console.log("Origin:", req.headers.origin);
+  next();
+});
+
+app.use(express.json()); // allows us to parse incoming requests from req.body
+app.use(cookieParser()); // allows us to parse incoming cookies
+
+app.use("/api/auth", authRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("/*splat", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+connectDb();
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
